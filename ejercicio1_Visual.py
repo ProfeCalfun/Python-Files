686383c551c27b48e259c6aa675ef8dd324f0547
ProfeCalfún <101536909+ProfeCalfun@users.noreply.github.com>
2023-04-16 16:05:13 -0400

    Clase 1 - Unidad II

---
+++ ejercicio1_Visual.py
@@ -0,0 +1,9 @@
+'''Autor del código: Francisco Calfún
+Esto es un comentario de párrafo
+El siguiente código muestra un mensaje y pide valores'''
+
+print("Hello World");
+nombre=input("Escriba su nombre, por favor:\n ");#En esta línea se pide el nombre.
+apellido=input("Escriba su apellido, por favor:\n");#En esta línea se pide el apellido.
+edad=int(input("Ingrese su edad, por favor: \n"))#en esta línea se pide la edad como valor entero.
+print(f"Su nombre es: {nombre} \nSu apellido es: {apellido}\nSu edad es: {edad}");#En esta línea se muetsra un mensaje con las variables pedidas más arriba
---
+++ ejercicioEcuacion.py
@@ -0,0 +1,8 @@
+'''
+Autor: Francisco Calfún
+Este código resuelve una ecuación matemática'''
+
+print("---------Ecuación cuadrática---------\n");
+numX=float(input("ingrese el valor de X: "));
+ecuacion=float((numX**2 + 3*numX + 1)/4);
+print(f"El resultado de la ecuación, reemplazando x po {numX} es: {ecuacion}")
